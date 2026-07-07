53e24c2da4d7a9e804e01acb25eac65e544ca749
jimyungsung-ai <jimyungsung@gmail.com>
2026-07-07 17:24:16 +0700

    Fix API: includeFiles db.json + safe store init

--- api/_store.js
+++ api/_store.js
@@ -1,10 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict'
+
 const { readFileSync } = require('fs')
 const { join } = require('path')
 
 if (!global.__store) {
-  const db = JSON.parse(readFileSync(join(__dirname, '../db.json'), 'utf8'))
+  let db
+  try {
+    // In Vercel, process.cwd() is the project root (/var/task)
+    db = JSON.parse(readFileSync(join(process.cwd(), 'db.json'), 'utf8'))
+  } catch {
+    db = { spots: [], votes: [], comments: [] }
+  }
   global.__store = {
-    spots:    JSON.parse(JSON.stringify(db.spots)),
+    spots:    JSON.parse(JSON.stringify(db.spots    || [])),
     votes:    JSON.parse(JSON.stringify(db.votes    || [])),
     comments: JSON.parse(JSON.stringify(db.comments || [])),
     _nextId:  20000,
